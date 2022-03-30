20b8ad2349109a0c5dc8232f0e3fdab0efae7e55
Amoy N <moyanich@users.noreply.github.com>
2022-03-30 17:05:40 -0500

    Updates

--- README.md
+++ README.md
@@ -55,13 +55,14 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Flexbox
 - CSS Grid
 - Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
+
 
 **Note: These are just examples. Delete this note and replace the list above with your own choices**
 
 ### What I learned
+
+Smooth tranistions
+Account for no selected rating.
 
 Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,100 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-   
+"use strict";
+
+/*
+TO DO 
+1. Smoother transitions
+2. give error when  no rating is selected
+*/
+
+const submit = document.querySelector('#submit');
+const ratingScale = document.querySelector('.rating-scale');
+const ratingOptions = ratingScale.children;
+
+console.log(ratingOptions);
+let selection;
+
+function makeSelection(e) {
+
+    if (e.target.classList.contains("rating-scale")) console.log('me');;
+
 }
 
+ratingScale.addEventListener("click", makeSelection);
+
+/*
+document.querySelector('#submit').addEventListener('click', selected);
+
+function selected() {
+    document.querySelector('.rating-state').classList.add('hidden');
+    document.querySelector('.state').classList.remove('hidden');
+}
+
+*/
+
+
+
+/*
+const rating = document.querySelector(".rating");
+const thanks = document.querySelector(".thanks");
+const ratingScale = document.querySelector(".rating-scale");
+const ratingOptions = ratingScale.children;
+const submit = document.querySelector(".submit");
+const userRating = document.querySelector(".user-rating");
+
+let selection;
+function select(e) {
+  if (e.target.classList.contains("rating-scale")) return;
+  for (let i = 0; i < ratingOptions.length; i++) {
+    ratingOptions[i].classList.remove("selected");
+  }
+  e.target.classList.add("selected");
+  selection = +e.target.textContent;
+}
+
+function shake() {
+  submit.classList.add("shake");
+  setTimeout(() => submit.classList.remove("shake"), 501);
+}
+
+function switchState() {
+  if (!selection) {
+    shake();
+  } else {
+    rating.style.opacity = 0;
+    rating.addEventListener("transitionend", function () {
+      rating.classList.add("hidden");
+      thanks.classList.remove("hidden");
+      setTimeout(() => (thanks.style.opacity = 1));
+    });
+    userRating.textContent = `You selected ${selection} out of 5`;
+  }
+}
+
+ratingScale.addEventListener("click", select);
+submit.addEventListener("click", switchState);
+
+*/
+
+
+
+/*
+
+
+
+const selected = () => {
+    document.querySelector('.rating-state').classList.add('hidden');
+    document.querySelector('.state').classList.remove('hidden');
+}
+
+
+document.querySelector('#item1').addEventListener('click', selectOne);
+document.querySelector('#item2').addEventListener('click', selectOne);
+document.querySelector('#item3').addEventListener('click', selectOne);
+document.querySelector('#item4').addEventListener('click', selectOne);
+document.querySelector('#item5').addEventListener('click', selectOne);
+
+function selectOne() {
+    document.querySelector('.user-rating').innerText = 1;
+}
+
+*/
